c67f94f49cf0708029560b78cad75e9a3846aac6
ILLUSION <76027489+GLX-ILLUSION@users.noreply.github.com>
2023-12-08 19:19:42 -0300

    no more necessary json file in disk

--- Example.cpp
+++ Example.cpp
@@ -1,20 +1,6 @@
 #include "offsets.hpp"
-#pragma comment(lib,"urlmon.lib") //for urldownload
 
 int main(int argc, char** argv)
 {
-
-	const char* url = "https://raw.githubusercontent.com/GLX-ILLUSION/offsets/main/offsets.json";
-	HRESULT result = URLDownloadToFileA(nullptr, url, "offsets.json", 0, nullptr);
-	if (result != S_OK)
-	{
-		std::cout << "[!] Could not update offsets. Error code: " << std::hex << result << std::endl;
-	}
-	else
-	{
-		std::cout << "[+] Offsets successfully downloaded." << std::endl;
-	}
-
-
 	offsets::initialize();
 }
